c0a9640129c7e98bf53e2f6e3fa3f9f0cad76dfb
Emmanuel Lampe <mail@emmanuel-lampe.de>
2021-01-12 01:38:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# vue 2 + tailwind
+
+Vue 2 CLI Starter with tailwind 2 and removed useless boilercode
+
+## Added
+- tailwindcss - 2.0.2
+- @tailwindcss/forms - 0.2.1
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="container mx-auto text-center">
+      <h4 class="text-5xl font-medium text-blue-800 my-4">
+        Vue 2 + Tailwind 2
+      </h4>
+      <a
+        class="text-xl text-blue-900 hover:underline"
+        href="https://github.com/rexlManu/vue-cli-2-tailwind-starter"
+        >vue-cli-2-tailwind-starter</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+body {
+  @apply bg-gray-50;
+}
+</style>
